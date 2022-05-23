f9a16f8ce5a99fa82308860f47b83bbe52307ba8
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-05-23 17:37:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 Status = "";
+ojects=[];
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -13,6 +14,31 @@ function setup() {
 
 function draw(){
     image(video, 0, 0, 480, 480);
+    if(status != ""){
+        objectDetector.detect(video, gotResults);
+        for(i = 0; i < objects.length; i++)
+        {
+        document.getElementbyId("status").innerHTML = "Status : Objects Detected";
+            document.getElementbyId("number_of_onjects").innerHTML = "Number of objects detected are : "+ objects.length;
+            
+            fill("red");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function gotResults(error, results)
+{
+if(error){
+console.error(error);
+}
+ else{
+ console.log(results);
+     objects=results;
+ }   
 }
 
 
